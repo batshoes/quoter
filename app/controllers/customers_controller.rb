88ca7b2ada59b678a43f6ceaf4814945ef529929
James Middlemiss <james.o.middlemiss@gmail.com>
2017-03-31 15:31:52 +1100

    adds edit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -21,6 +21,19 @@ class CustomersController < ApplicationController
     end
   end
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update_attributes(customer_params)
+      redirect_to customer_path(@customer), flash: {notice: "Updated!"}
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def customer_params
